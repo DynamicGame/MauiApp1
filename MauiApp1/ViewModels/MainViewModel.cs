72c1017ae7403370f019d0465a0f7ba1899e5ded
DynamicGame <soliuabdulbasit03@gmail.com>
2024-01-21 15:47:14 +0000

    Add project files.

---
+++ MauiApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MauiApp1.ViewModels
+{
+   public partial class MainViewModel : ObservableObject
+    {
+        IConnectivity connectivity;
+        public MainViewModel(IConnectivity connectivity) 
+        {
+            Items = new ObservableCollection<string>();
+            this.connectivity = connectivity;
+        }  
+        [ObservableProperty]
+        ObservableCollection<string> items;
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]
+        async Task Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+                return;
+            if(connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("Sorry", "No Internet Service" , "Ok");
+            }
+            Items.Add(Text);
+            //Add our item
+            Text = string.Empty; 
+        }
+
+        [RelayCommand]
+        void Delete(string s)
+        {
+            if (Items.Contains(s))
+            {
+                Items.Remove(s);
+            }
+        }
+
+        [RelayCommand]
+        async Task Tap(string s)
+        {
+            await Shell.Current.GoToAsync($"{nameof(DetailPage)}?id={s}");
+        }
+    }
+}
